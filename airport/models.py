bc7c2bc662b51f39785031833ec28181b4e21d23
Marharyta Zazulia <zazmargo@gmail.com>
2024-12-27 15:11:52 -0300

    create new models City y Country according to project diagram

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    class Meta:
+        verbose_name_plural = "countries"
+
+    def __str__(self: "Country") -> str:
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=63)
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE,
+        related_name="cities"
+    )
+
+    class Meta:
+        verbose_name_plural = "cities"
+
+    def __str__(self: "City") -> str:
+        return self.name
